f9876c5f4dfa9dbc025da24392db4d19ca09bf66
Gasparyan1999 <gasparyan.george1999@gmail.com>
2022-09-02 14:41:03 +0400

    NodeApi

---
+++ app.js
@@ -0,0 +1,35 @@
+import express, { query } from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import pool from "./Data/data.js";
+
+const PORT = process.env.port || 3000;
+const app = express();
+app.use(cors());
+
+app.use(bodyParser.json());
+
+app.get("/", async function (req, res) {
+  const cards = await pool.query("SELECT * FROM cards");
+  console.log(cards.rows);
+  res.send(cards.rows);
+});
+
+app.post("/", async function (req, res) {
+  if (req.body[0].num) {
+    const value = [req.body[0].id, req.body[0].num];
+    const card = await pool.query(
+      `INSERT INTO cards VALUES
+      ('${value[0]}'::text,'${value[1]}'::integer) RETURNING *`
+    );
+    res.json(card.rows);
+  } else {
+    const value = [req.body[0]];
+    const card = await pool.query(`DELETE FROM cARDS WHERE id = '${value}'`);
+    res.json(card.rows);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server start in ${PORT}`);
+});
